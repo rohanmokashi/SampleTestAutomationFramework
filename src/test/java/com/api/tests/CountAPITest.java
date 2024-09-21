962a08086791fb72d7f52287187c2802940152fc
rohanmokashi <rohan.mokashi7@gmail.com>
2024-09-21 07:45:15 +0530

    Update CountAPITest.java
    committed 3rd line in repo to get conflict

--- src/test/java/com/api/tests/CountAPITest.java
+++ src/test/java/com/api/tests/CountAPITest.java
@@ -5,6 +5,7 @@ public class CountAPITest {
 	public static void main(String args[]) {
 		System.out.println("Count API Test");
 		System.out.println("Conflict line2");
+		System.out.println("Conflict line3 from remote repo");
 		
 	}
 
